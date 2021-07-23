93f842685cf8e5d0a31d45ab5283c4fc429c4f10
Daniel Marques <daniel.brz2009@gmail.com>
2021-07-23 19:59:20 -0300

    First commit - Project scaffold

---
+++ knexfile.ts
@@ -0,0 +1,42 @@
+import 'dotenv/config';
+import { resolve } from 'path';
+
+module.exports = {
+  client: 'pg',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    ssl:
+      process.env.NODE_ENV === 'production'
+        ? { rejectUnauthorized: false }
+        : undefined,
+  },
+  pool: {
+    min: 2,
+    max: 10,
+  },
+  migrations: {
+    extension: 'ts',
+    tableName: 'migrations',
+    directory: resolve(
+      __dirname,
+      process.env.NODE_ENV === 'production' ? 'dist' : '',
+      'src',
+      'shared',
+      'knex',
+      'migrations'
+    ),
+  },
+  seeds: {
+    directory: resolve(
+      __dirname,
+      process.env.NODE_ENV === 'production' ? 'dist' : '',
+      'src',
+      'shared',
+      'knex',
+      'seeds'
+    ),
+  },
+};
